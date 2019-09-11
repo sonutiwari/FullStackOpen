744fd6f6d9e2c5dfe4c8bf271edb93b634af787d
sonu-tiwari-au2 <tiwarisonu05081996@gmail.com>
2019-09-11 14:26:15 +0530

    react initial setup and practice

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const Header = (props) => {
+    return (
+        <h1>{props.course}</h1>
+    );
+}
+
+const Content = (props) => {
+    return (<div>
+        <Part part = {props.parts[0]}/>
+        <Part part = {props.parts[1]}/>
+        <Part part = {props.parts[2]}/>
+    </div>);
+}
+
+const Total = (props) => {
+    const parts = props.parts;
+    const total = parts.reduce((sum, element) => sum + element.exercises, 0);
+    return (<p>Number of exercises {total}</p>)
+}
+
+const Part = (props) => {
+    const {name, exercises} = props.part;
+    return (<p>{name} {exercises}</p>)
+};
+const App = () => { 
+  const parts = [
+      {
+          name : 'Fundamentals of React',
+          exercises : 10
+      },
+      {
+        name : 'Using props to pass data',
+        exercises : 7
+      },
+      {
+        name : 'State of a component',
+        exercises : 14
+      }
+  ];
+  const course = 'Half Stack application development';
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts = {parts}/>
+      <Total parts = {parts}/>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
